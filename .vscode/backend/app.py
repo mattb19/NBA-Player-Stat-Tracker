f71a26fd20f487c8e6ba678b446b93c3418ad7d4
mattb19 <80365359+mattb19@users.noreply.github.com>
2022-09-26 19:53:10 -0400

    organized frontend and backend files

---
+++ .vscode/backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for, render_template
+from flask_cors import CORS
+from WebScraper import *
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r'/*':{'origins': 'http://localhost:8080', "allow_headers" : "Access-Control-Allow-Origin"}})
+
+
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
