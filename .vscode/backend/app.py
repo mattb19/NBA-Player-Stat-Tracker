8b5c710011e83d87b4a8743d95bf303f23278724
trentsalas <tesalas@datacor.com>
2022-09-28 14:33:47 -0400

    test

--- .vscode/backend/app.py
+++ .vscode/backend/app.py
@@ -17,14 +17,14 @@ CORS(app, resources={r'/*':{'origins': 'http://localhost:8080', "allow_headers" 
 
 
 
-Mike = RegAvg("Michael", "Jordan", 50, 23, 'lakers', 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
-Steph = RegAvg("Steph", "Curry", 60, 69, 'bulls', 20, 15, 17, 15, 13, 9, 2, 8, 10, 7, 6, 5)
-LeBron = RegAvg("LeBron", "James", 72, 12, 'lakers', 30, 18, 11, 18, 13, 9, 3, 8, 22, 1, 6, 8)
-Kev = RegAvg("Kevin", "Durant", 74, 74, 'bulls', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
-Kev1 = RegAvg("Kevin", "Durant", 74, 74, 'bulls', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
-Kev2 = RegAvg("Kevin", "Durant", 74, 74, 'bulls', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
-Kev3 = RegAvg("Kevin", "Durant", 74, 74, 'bulls', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
-Kev4 = RegAvg("Kevin", "Durant", 74, 74, 'bulls', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
+Mike = RegAvg("Michael", "Jordan", 50, 23, 'nuggets', 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
+Steph = RegAvg("Steph", "Curry", 60, 69, 'pistons', 20, 15, 17, 15, 13, 9, 2, 8, 10, 7, 6, 5)
+LeBron = RegAvg("LeBron", "James", 72, 12, 'warriors', 30, 18, 11, 18, 13, 9, 3, 8, 22, 1, 6, 8)
+Kev = RegAvg("Kevin", "Durant", 74, 74, 'rockets', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
+Kev1 = RegAvg("Kevin", "Durant", 74, 74, 'pacers', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
+Kev2 = RegAvg("Kevin", "Durant", 74, 74, 'clippers', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
+Kev3 = RegAvg("Kevin", "Durant", 74, 74, 'grizzlies', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
+Kev4 = RegAvg("Kevin", "Durant", 74, 74, 'heat', 30, 67, 77, 72, 73, 1, 3, 7, 50, 7, 9, 7)
 players = [Mike, Steph, LeBron, Kev, Kev1, Kev2, Kev3, Kev4]
 newPlayerList = PlayerList()
 newPlayerList.setPlayerList(players)
